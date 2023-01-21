606f8e02819a1b44c9b05513d4a0209f6ef6a57e
anshit-1223 <anshit1223@gmail.com>
2023-01-21 21:03:12 +0530

    File created and Basic API created

---
+++ data.js
@@ -0,0 +1,6 @@
+data=[{name:'Anshit Verma',course:'MCA'},
+    {name:'Abhishek Gupta',course:'MCA'},
+    {name:'Aman Kumar',course:'MCA'}
+
+];
+module.exports=data;
---
+++ index.js
@@ -0,0 +1,23 @@
+// const http = require('http');
+// const colors=require('colors');
+// const data=require('./data');
+// http.createServer((req,res)=>{
+//     //res.write("Server Started");
+//     res.writeHead(200,{'Content-Type':'application/json'});
+//     res.write(JSON.stringify(data));
+//     res.end();
+//     console.log("Server Running...".green);
+// }).listen(8000);
+
+const fs = require('fs');
+
+const input=process.argv;
+
+if(input[2]=='add'){
+    fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]=='remove'){
+    fs.unlinkSync(input[3]);
+}
+else
+    console.log("Invalid Operation");
