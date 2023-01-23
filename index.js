2f7692408999a3f64cb2174d1f781e826926b076
anshit-1223 <anshit1223@gmail.com>
2023-01-23 21:13:54 +0530

    creating files and printting on console

--- index.js
+++ index.js
@@ -9,15 +9,35 @@
 //     console.log("Server Running...".green);
 // }).listen(8000);
 
+
+
+// const fs = require('fs');
+
+// const input=process.argv;
+
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3],input[4]);
+// }
+// else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3]);
+// }
+// else
+//     console.log("Invalid Operation");
+
+
 const fs = require('fs');
+const path = require('path');
 
-const input=process.argv;
+const dirpath=path.join(__dirname,'files');
+for (let index = 0; index < 5; index++) {
+    fs.writeFileSync(dirpath+`/Hello${index}.txt`,'Hii this is file');
+    //fs.writeFileSync(dirpath+"/Hello"+index+".txt","Hii this is file");
+    
+}
 
-if(input[2]=='add'){
-    fs.writeFileSync(input[3],input[4]);
-}
-else if(input[2]=='remove'){
-    fs.unlinkSync(input[3]);
-}
-else
-    console.log("Invalid Operation");
+fs.readdir(dirpath,(err,files)=>{
+    files.forEach((item) => {
+        console.log(item);
+        
+    });
+})
